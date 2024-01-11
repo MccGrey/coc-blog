8c816b01902b442ae8fffcb395262f71dddeda2b
MccGrey <chuxzzie01@yahoo.com>
2024-01-11 08:23:02 +0100

    create data.js

---
+++ src/data.js
@@ -0,0 +1,52 @@
+import Thumbnail1 from "./images/thumbnail1.png";
+import Thumbnail2 from "./images/thumbnail2.png";
+import Thumbnail3 from "./images/thumbnail3.png";
+import Thumbnail4 from "./images/thumbnail4.png";
+import Thumbnail5 from "./images/thumbnail5.png";
+
+export const DUMMY_POSTS = [
+  {
+    id: "1",
+    thumbnail: Thumbnail1,
+    category: "doctrinal teachings",
+    title: "What does God mean to you",
+    desc: "What is God to you. How can you evaluate your love and relationship with God? What is God to you. How can you evaluate your love and relationship with God?",
+    authorID: 3,
+  },
+
+  {
+    id: "2",
+    thumbnail: Thumbnail2,
+    category: "doctrinal teachings",
+    title: "The crusades",
+    desc: "Having known that crusade in our study context refers to a holy war, our study is not going to focus on various political wars of the Roman Catholic Church against Muslims and other external forces but the crusades the Roman Catholic Church carried out against Christian oppositions to the authority of the Pope and Roman Catholic doctrines in the guise of fighting heresies.",
+    authorID: 1,
+  },
+
+  {
+    id: "3",
+    thumbnail: Thumbnail3,
+    category: "doctrinal teachings",
+    title: "Restoration movement",
+    desc: "Understanding these two key periods in Christian history will open the horizon for purposeful bible studies which will help us to understand that God only wants perfect Christian unity in doctrines and practices rather than denominations",
+    authorID: 3,
+  },
+
+  {
+    id: "4",
+    thumbnail: Thumbnail4,
+    category: "doctrinal teachings",
+    title: "Some key depatures",
+    desc: "When a train derails from its rail, it is always disastrous. That is exactly what happened to the Church of God when it derailed from its original faith to Catholicism. Today, we are going to highlight some key departures from the original faith by the Romanized church",
+    authorID: 3,
+  },
+
+  {
+    id: "5",
+    thumbnail: Thumbnail5,
+    category: "doctrinal teachings",
+    title: "Reformation and restoration",
+    desc: "We shall also see some the erroneous doctrines and practices that required to be restored to their original Scriptural patterns and their eventual restorations. We shall also see some the erroneous doctrines and practices that required to be restored to their original Scriptural patterns and their eventual restorations.",
+    authorID: 3,
+  },
+];
